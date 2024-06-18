11603e80bc129964f465a2f06d55e94f29cd9dd9
QDXG-CXK <63047639+QDXG-CXK@users.noreply.github.com>
2024-06-18 09:03:46 +0800

    Update read_bin.cpp

--- scripts/read_bin.cpp
+++ scripts/read_bin.cpp
@@ -2,19 +2,17 @@
 #include <fstream>
 #include <vector>
 #include <string>
-#include <type_traits>
 
 // 模板函数读取二进制文件
 template<typename T>
 std::vector<T> readBinaryFile(const std::string& filePath, size_t fileSize) {
-    static_assert(std::is_trivially_copyable<T>::value, "Data type must be trivially copyable");
 
     std::vector<T> data(fileSize / sizeof(T));
 
     // 打开文件
     std::ifstream file(filePath, std::ios::binary);
     if (!file.is_open()) {
-        std::cerr << "无法打开文件: " << filePath << std::endl;
+        std::cerr << "can't open file " << filePath << std::endl;
         return std::vector<T>();
     }
 
@@ -23,7 +21,7 @@ std::vector<T> readBinaryFile(const std::string& filePath, size_t fileSize) {
     
     // 检查是否读取成功
     if (!file) {
-        std::cerr << "读取文件失败或读取的字节数不正确。" << std::endl;
+        std::cerr << "failure." << std::endl;
         return std::vector<T>();
     }
 
