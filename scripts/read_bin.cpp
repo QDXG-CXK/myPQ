ab7c61922c4c20969e2e2ac8850e3f9fbce8e569
QDXG-CXK <63047639+QDXG-CXK@users.noreply.github.com>
2024-06-18 09:34:51 +0800

    Update read_bin.cpp

--- scripts/read_bin.cpp
+++ scripts/read_bin.cpp
@@ -33,7 +33,7 @@ std::vector<T> readBinaryFile(const std::string& filePath, size_t fileSize) {
 
 int main() {
     // 调用函数读取文件，将数据读取成uint8_t类型的向量
-    std::vector<int64_t> fileData = readBinaryFile<int64_t>("../data/float/gt_I.bin", 4000);
+    std::vector<int64_t> fileData = readBinaryFile<int64_t>("/home/algo/xdu/normal_cpu/myPQ/data/float/gt_I.bin", 4000);
     // 输出读取的数据（仅示例）
     for (size_t i = 0; i < fileData.size(); ++i) {
         if(i%5==0) std::cout<<std::endl;
@@ -42,7 +42,7 @@ int main() {
     std::cout << std::endl;
 
     // 调用函数读取文件，将数据读取成uint8_t类型的向量
-    std::vector<float> fileData2 = readBinaryFile<float>("../data/float/gt_D.bin", 2000);
+    std::vector<float> fileData2 = readBinaryFile<float>("/home/algo/xdu/normal_cpu/myPQ/data/float/gt_D.bin", 2000);
     // 输出读取的数据（仅示例）
     for (size_t i = 0; i < fileData2.size(); ++i) {
         if(i%5==0) std::cout<<std::endl;
